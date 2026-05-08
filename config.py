990ed76c6bb9eb557f2b2178a6e383a689a23a3d
Lbexsis06 <benchikhnassim10@gmail.com>
2026-05-08 23:32:59 +0200

    feat: add config file to centralize constants

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+EMBEDDING_MODEL_NAME = "paraphrase-multilingual-mpnet-base-v2"
+LLM_MODEL_NAME = "llama-3.3-70b-versatile"
+DB_PATH = "./tmdb_vector_db"
+COLLECTION_NAME = "films"
+CSV_PATH = "tmdb_5000_movies.csv"
